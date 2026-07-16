fa4bac4ec4d5fe556dc5fd3bb91e33a4c5a65b07
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2026-07-17 01:37:43 +0200

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'mvsl-shell-v1';
+const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  const isShellFile = SHELL_FILES.some((f) => url.pathname.endsWith(f.replace('./', '')));
+  if (isShellFile) {
+    event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  }
+});
